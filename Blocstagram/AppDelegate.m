6d66de677767dd58fd1204a39567804ed2e41303
Casey Ward <caseyward1@gmail.com>
2015-03-15 15:30:12 -0700

    Table View controller implemented

--- Blocstagram/AppDelegate.m
+++ Blocstagram/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "BLCImagesTableViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -18,6 +20,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+      self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BLCImagesTableViewController alloc] init]];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
---
+++ Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Casey Ward on 3/14/15.
+//  Copyright (c) 2015 Casey Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
